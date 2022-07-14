0bf0361c29658172736ca80ad84e703ec9826883
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-07-14 17:56:55 +0200

    add item task test

---
+++ src/localstore.test.js
@@ -0,0 +1,35 @@
+import { JSDOM } from 'jsdom';
+import localstore from './mocks/localstore.js';
+import displayhtml from './displayhtml.js';
+
+const dom = new JSDOM();
+global.document = dom.window.document;
+global.window = dom.window;
+
+const myhtml = () => {
+  document.body.innerHTML = `<ul id="todo-list">
+      <li>Today's To Do<i class="fa-solid fa-arrows-rotate"></i></li>
+      <li class="add-item"><input type="text" id="inp-add-item" placeholder="Add to your list ..."></li>
+      <li class="btn"><a href="">Clear all Completed</a></li>
+    </ul>`;
+  return document;
+};
+
+describe('Add Method', () => {
+  test('Add to the list', () => {
+    expect(localstore.add({ target: { value: 'ABC' } })).toBe(1);
+  });
+
+  test('Add one new item to the list', () => {
+    myhtml();
+    displayhtml.addListItem({
+      description: 'First Item',
+      completed: false,
+      index: 1,
+    });
+    const list = document.querySelectorAll('#todo-list li');
+    expect(list).toHaveLength(4);
+  });
+});
+
+
